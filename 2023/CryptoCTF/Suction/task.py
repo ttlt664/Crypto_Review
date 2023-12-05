cf44f7ca5af1943d63c9eb79ace937b726182217
23 <syldyx2020@163.com>
2023-12-05 15:40:24 +0800

    cryptoctf2023

---
+++ 2023/CryptoCTF/Suction/task.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2023/12/5 15:34
+#!/usr/bin/env python3
+
+from Crypto.Util.number import *
+flag = b"CCTF{this_is_just_a_test_flag}"
+
+def keygen(nbit, r):
+	while True:
+		p, q = [getPrime(nbit) for _ in '__']
+		e, n = getPrime(16), p * q
+		phi = (p - 1) * (q - 1)
+		if GCD(e, phi) == 1:
+			N = bin(n)[2:-r]
+			E = bin(e)[2:-r]
+			PKEY = N + E
+			pkey = (n, e)
+			return PKEY, pkey
+
+def encrypt(msg, pkey, r):
+	m = bytes_to_long(msg)
+	n, e = pkey
+	c = pow(m, e, n)
+	C = bin(c)[2:-r]
+	return C
+
+r, nbit = 8, 128
+PKEY, pkey = keygen(nbit, r)
+print(f'PKEY = {int(PKEY, 2)}')
+FLAG = flag.lstrip(b'CCTF{').rstrip(b'}')
+enc = encrypt(FLAG, pkey, r)
+print(f'enc = {int(enc, 2)}')
